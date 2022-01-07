c281edd7b69a18f57db09df504fa166150f4ea20
devhowey <dev.howey@gmail.com>
2022-01-06 19:56:37 -0600

    set up reducer, actions, services

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+// define string constants to indicate type of action being performed
+
+export const CREATE_TUTORIAL= "CREATE_TUTORIAL";
+export const RETRIEVE_TUTORIALS = "RETRIEVE_TUTORIALS";
+export const UPDATE_TUTORIAL = "UPDATE_TUTORIAL";
+export const DELETE_TUTORIAL = "DELETE_TUTORIAL";
+export const DELETE_ALL_TUTORIALS = "DELETE_ALL_TUTORIALS";
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+// this will be helpful for combining auth, articles, comments, etc.
+import { combineReducers } from "redux";
+import tutorials from "./turoials";
+
+export default combineReducers({
+  tutorials,
+});
